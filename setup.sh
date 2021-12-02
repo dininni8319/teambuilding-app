d7c6906331d00709e53044db2608da81c5338ee2
Emidio Cruciani <cruciani@multidialogo.it>
2021-12-02 17:24:01 +0100

    FEAT/WLCM/US093S33-A | Aggiunta scelta repo custom in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,27 @@
 #bin/sh
 echo 'Installing application...'
 
+echo 'Do you want to pull code from a custom repo?'
+read -n 1 -s -r -p '(Y/N): ' confirm_custom_repo
+if [[ $confirm_custom_repo == [yY] ]]
+then
+  read -p 'Enter a custom location: ' repo_location
+else
+  echo 'No remote repo... yet'
+  exit 1
+fi
+
+current_dir=$PWD
+
+rm -rf Docker/app/source
+mkdir -p Docker/app/source
+cd Docker/app/source
+git clone $repo_location .
+cd $current_dir
+
 cp .env.dist .env
-docker compose up --build
+echo 'Edit the .env file with your variables'
+read -n 1 -s -r -p 'Press any key to continue when you are finished'
+echo ''
+docker compose up --build -d
+docker compose exec web python manage.py migrate
