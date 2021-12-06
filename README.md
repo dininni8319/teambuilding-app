830d17f0b01abc1e1f38053f09984f46d022ce96
Emidio Cruciani <cruciani@multidialogo.it>
2021-12-06 12:42:04 +0100

    Setup fix and other minor fixes

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Run the setup.sh
+2. Choose to use a custom report location for source, or use the default remote one
+3. Edit the environment variables in the .env file
--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #bin/sh
-echo 'Installing application...'
+echo "Installing application..."
 
-echo 'Do you want to pull code from a custom repo?'
-read -n 1 -s -r -p '(Y/N): ' confirm_custom_repo
+current_dir=${PWD}
+
+echo "Do you want to pull code from a custom repo?"
+read -n 1 -s -r -p "(Y/N): " confirm_custom_repo
+echo ""
 if [[ $confirm_custom_repo == [yY] ]]
 then
-  read -p 'Enter a custom location: ' repo_location
+  read -p "Enter a custom location: " repo_location
+  echo ""
 else
-  echo 'No remote repo... yet'
-  exit 1
+  repo_location="ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/tb_taste_purchase"
 fi
-
-current_dir=$PWD
 
 rm -rf Docker/app/source
 mkdir -p Docker/app/source
 cd Docker/app/source
-git clone $repo_location .
+git clone -b develop $repo_location .
 cd $current_dir
 
 cp .env.dist .env
-echo 'Edit the .env file with your variables'
-read -n 1 -s -r -p 'Press any key to continue when you are finished'
-echo ''
+echo "Edit the .env file with your variables"
+read -n 1 -s -r -p "Press any key to continue when you are finished"
+echo ""
+
 docker compose up --build -d
 docker compose exec web python manage.py migrate
+
+xdg-open http://localhost:8000
