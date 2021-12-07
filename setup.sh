1914b85e5ccbf0b7a1fb3a7958ec99feced101a8
Emidio Cruciani <cruciani@multidialogo.it>
2021-12-07 13:00:38 +0100

    Fixed setup.sh UX

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #bin/sh
 echo "Installing application..."
 
-current_dir=${PWD}
+provisioning_dir=${PWD}
 
 echo "Do you want to pull code from a custom repo?"
 read -n 1 -s -r -p "(Y/N): " confirm_custom_repo
@@ -9,7 +9,6 @@ echo ""
 if [[ $confirm_custom_repo == [yY] ]]
 then
   read -p "Enter a custom location: " repo_location
-  echo ""
 else
   repo_location="ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/tb_taste_purchase"
 fi
@@ -17,16 +16,20 @@ fi
 rm -rf Docker/app/source
 mkdir -p Docker/app/source
 cd Docker/app/source
-git clone -b develop $repo_location .
-cd $current_dir
+git clone $repo_location .
 
-cp .env.dist .env
+echo "Available branches: "
+git branch -a
+
+read -p "Enter the starting branch name: " branch_name
+git checkout $branch_name
+
+cp "${provisioning_dir}/.env.dist" "${provisioning_dir}.env"
 echo "Edit the .env file with your variables"
 read -n 1 -s -r -p "Press any key to continue when you are finished"
 echo ""
 
-export COMPOSE_FILE="${PWD}/docker-compose.yml"
-cd Docker/app/source
+export COMPOSE_FILE="${provisioning_dir}/docker-compose.yml"
 
 docker compose up --build -d
 docker compose exec web python manage.py migrate
