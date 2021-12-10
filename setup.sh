513656dd21b96809890eaab876d3f38e2e9d1589
Emidio Cruciani <cruciani@multidialogo.it>
2021-12-10 12:12:22 +0100

    Fix .env copy in setup.sh

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ git branch -a
 read -p "Enter the starting branch name: " branch_name
 git checkout $branch_name
 
-cp "${provisioning_dir}/.env.dist" "${provisioning_dir}.env"
+cp "${provisioning_dir}/.env.dist" "${provisioning_dir}/.env"
 echo "Edit the .env file with your variables"
 read -n 1 -s -r -p "Press any key to continue when you are finished"
 echo ""
