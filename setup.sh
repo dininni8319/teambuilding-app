4989df05efd123c2ab5adaa04177b233295b2db5
Emidio Cruciani <cruciani@multidialogo.it>
2021-12-13 11:51:30 +0100

    refactor/131221/001-setup

--- setup.sh
+++ setup.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
+(
+  set -o errexit
+  set -o pipefail
+  set -o nounset
 
-provisioning_dir="${PWD}"
-repo_source="ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/tb_taste_purchase"
-repo_destination="${provisioning_dir}/Docker/app/source"
+  provisioning_dir="${PWD}"
+  repo_source="ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/tb_taste_purchase"
+  repo_destination="${provisioning_dir}/Docker/app/source"
 
-echo "Installing application..."
+  echo "Installing application..."
 
-cp -p "${provisioning_dir}/.env.dist" "${provisioning_dir}/.env"
+  cp -p "${provisioning_dir}/.env.dist" "${provisioning_dir}/.env"
 
-echo "Edit the .env file with your variables"
-read -n 1 -s -r -p "Press any key to continue when you are finished"
-echo ""
+  echo "Edit the .env file with your variables"
+  read -n 1 -s -r -p "Press any key to continue when you are finished"
+  echo ""
 
-set -o allexport; source .env; set +o allexport
+  set -o allexport
+  source .env
+  set +o allexport
 
-echo "Cloning repository..."
-rm -rf "${repo_destination}"
-mkdir -p "${repo_destination}"
-cd "${repo_destination}"
-git clone "${repo_source}" "${repo_destination}"
+  echo "Cloning repository..."
+  rm -rf "${repo_destination}"
+  mkdir -p "${repo_destination}"
+  cd "${repo_destination}"
+  git clone "${repo_source}" "${repo_destination}"
 
-echo "Available branches: "
-git fetch origin
-git branch -a
+  echo "Available branches: "
+  git fetch origin
+  git branch -a
 
-read -p "Enter the starting branch name: " branch_name
-git checkout "${branch_name}"
+  read -p "Enter the starting branch name: " branch_name
+  git checkout "${branch_name}"
 
-export COMPOSE_FILE="${provisioning_dir}/docker-compose.yml"
+  export COMPOSE_FILE="${provisioning_dir}/docker-compose.yml"
 
-docker compose up --build --force-recreate -d
-docker compose exec web python manage.py migrate
+  docker compose up --build --force-recreate -d
+  # docker compose exec web python manage.py migrate
 
-ping "${HTTP_HOST}:${HTTP_PORT}" | echo "Cannot reach ${HTTP_HOST}:${HTTP_PORT}, please ensure it in your etc/hosts file" & exit 1
+  ping -c 3 "${HTTP_HOST}" || exit 1
 
-xdg-open "http://${HTTP_HOST}:${HTTP_PORT}"
+  xdg-open "http://${HTTP_HOST}:${HTTP_PORT}"
+)
